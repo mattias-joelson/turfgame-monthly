3cdf333ceb0af15a9c8ff49b6f8ed8039421aab9
Mattias Joëlson <mattias@joelson.org>
2025-02-01 23:37:05 +0100

    Statistics round 175 - January 2025

--- src/test/java/org/joelson/turf/zundin/MonthlyTest.java
+++ src/test/java/org/joelson/turf/zundin/MonthlyTest.java
@@ -8,7 +8,7 @@ public class MonthlyTest {
     private static final int ROUND = 113;
 
     public static Monthly getMonthly() throws Exception {
-        return URLReaderTest.readProperties("monthly_0beroff_round169.html", s -> Monthly.fromHTML(OBEROFF, ROUND, s));
+        return URLReaderTest.readProperties("monthly_0beroff_round175.html", s -> Monthly.fromHTML(OBEROFF, ROUND, s));
     }
 }
 
