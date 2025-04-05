150d0d52b0b1a6240ed607f17ead94f0ac9ed0ef
Mattias Joëlson <mattias@joelson.org>
2025-04-05 15:53:00 +0200

    Store last zundin file as backup (for fairy, etc).

--- zundinmonthly.bat
+++ zundinmonthly.bat
@@ -5,5 +5,6 @@ set round=176
 set filename=monthly_%user%_round%round%.html
 
 call mvn exec:java -Dexec.mainClass="org.joelson.turf.zundin.Monthly" -Dexec.args="user=%user% round=%round%"
-move /Y %filename% src\test\resources
-dir /od src\test\resources\%filename%
+move src\test\resources\%filename% src\test\resources\%filename%.bak
+move %filename% src\test\resources
+dir /od src\test\resources\%filename%*
