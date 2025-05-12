9b999ad41d2b2ea7cbfb76cb9ee6d309e274a04d
Mattias Joëlson <mattias@joelson.org>
2025-05-12 07:45:03 +0200

    A small pause between each zones download.

--- zones.bat
+++ zones.bat
@@ -1,7 +1,9 @@
 @echo off
 
 call mvn exec:java -Dexec.mainClass="org.joelson.turf.turfgame.apiv4.Zones"
+timeout /t 5 /nobreak
 call mvn exec:java -Dexec.mainClass="org.joelson.turf.turfgame.apiv5.Zones"
+timeout /t 5 /nobreak
 call mvn exec:java -Dexec.mainClass="org.joelson.turf.turfgame.apiv6.Zones"
 dir zones-all.v?.json
 copy zones-all.v?.json src\test\resources
