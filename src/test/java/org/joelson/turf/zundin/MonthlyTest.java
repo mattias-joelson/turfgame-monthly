d4a46109f09d099415629b5a07dd5547393be456
Mattias Joëlson <mattias@joelson.org>
2026-06-07 09:43:42 +0200

    Statistics round 191 - May 2026

--- src/test/java/org/joelson/turf/zundin/MonthlyTest.java
+++ src/test/java/org/joelson/turf/zundin/MonthlyTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class MonthlyTest {
 
     private static final String NICK = "0beroff";
-    private static final int ROUND = 190;
+    private static final int ROUND = 191;
     private static final String FILENAME = String.format("monthly_%s_round%d.html", NICK, ROUND);
 
     public static Monthly getMonthly() throws IOException {
